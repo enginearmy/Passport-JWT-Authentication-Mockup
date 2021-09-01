31ba0ca6b75da1a7a0bbcaad033a009d16f699fa
Steve Hamilton <steve@enginearmy.com>
2021-09-01 11:20:55 -0400

    add express with simple app

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+import express from 'express'
+
 import JWT from 'jsonwebtoken'
 import passport from 'passport'
 
-import JwtStrategy, { ExtractJwt } from 'passport-jwt'
+import { Strategy, ExtractJwt } from 'passport-jwt'
+
+const app = express();
 
 
 const JWT_SECRET = '2390239847223483439428734'
@@ -15,31 +19,27 @@ const testToken = JWT.sign(user, JWT_SECRET)
 
 
 //Mock getting a token from a req
-const mockExtractor = (req) => {
-    return testToken
-}
-
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret';
-
+const mockExtractor = (req) => testToken
 
 const jwtStrategyOptions = {
-    jwtFromRequest = mockExtractor,
-    secretOrKey = JWT_SECRET
+    jwtFromRequest: mockExtractor,
+    secretOrKey: JWT_SECRET
 }
 
 
-const jwtStrategy = new JwtStrategy(
-
+const jwtStrategy = new Strategy(
     jwtStrategyOptions, (jwt_payload, done) => {
-        if(err){
-            return done(err, false)
-        }
-        if(user){
-
-        }
+        //console.log(jwt_payload)
+        done(null, jwt_payload);
     }
 )
 
 
-passport.use(new JwtStrategy)
+passport.use(jwtStrategy)
+app.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    console.log('Route hit!')
+})
+
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}.`))
