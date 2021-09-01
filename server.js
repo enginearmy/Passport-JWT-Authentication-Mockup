c0677d39c20745c210d3c9ad627ef156fa612e0f
Steve Hamilton <steve@enginearmy.com>
2021-09-01 10:51:12 -0400

    add jwt creation from mock user

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("hellow donkey")
+import JWT from 'jsonwebtoken'
+import passport from 'passport'
+
+import JwtStrategy, { ExtractJwt } from 'passport-jwt'
+
+
+const JWT_SECRET = '2390239847223483439428734'
+
+const user = {
+    id: 1,
+    name: 'person1'
+}
+
+const testToken = JWT.sign(user, JWT_SECRET)
+
+console.log(testToken)
