0e8e3cc3782f01a8cc8d592445bd3e39baf91d7f
Steve Hamilton <steve@enginearmy.com>
2021-09-01 12:17:23 -0400

    define jwt-strategy for passport

--- passport/jwtStrategy.js
+++
@@ -1,4 +0,0 @@
-import JWT from 'jsonwebtoken'
-import { Strategy } from 'passport-jwt'
-
-const JWT_SECRET = process.env.JWT_SECRET
---
+++ passport/jwtStrategyInitializer.js
@@ -0,0 +1,33 @@
+import { Strategy,  ExtractJwt } from 'passport-jwt'
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+//Define how the token will be extracted from the req by the jwt strategy
+const jwtExtractor = ExtractJwt.fromAuthHeaderAsBearerToken();
+
+
+//Define options for the strategy
+const jwtStrategyOptions = {
+    jwtFromRequest: jwtExtractor,
+    secretOrKey: JWT_SECRET
+}
+
+
+//Define how the token will be processed and used for authentication
+const jwtStrategy = () => {
+    return new Strategy(jwtStrategyOptions, (jwt_payload, done) => {
+
+        if(jwt_payload){
+            console.log(`SUCCESS ===> Payload: ${jwt_payload.name}`)
+            done(null, jwt_payload)
+        }else{
+            throw new Error("JWT-Strategy did not find a jwt_payload!")
+        }
+    })
+}
+
+//Add the strategy to passport
+const jwtStrategyInitializer = (passport) => 
+    passport.use(jwtStrategy);
+
+export default jwtStrategyInitializer;
